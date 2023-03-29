af5b7d6044237b157a6841a342411e55e7849b58
0004203 <youpomian@suiyi.com.cn>
2023-03-30 00:21:21 +0800

    layout init

---
+++ modules/layout/index.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
+import classnames from 'classnames';
+import styles from './index.module.scss';
+
+const { Header, Sider, Content } = Layout;
+const App: React.FC = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <Layout className={styles.layout}>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="logo">AI GPT</div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <UserOutlined />,
+              label: 'nav 1',
+            },
+            {
+              key: '2',
+              icon: <VideoCameraOutlined />,
+              label: 'nav 2',
+            },
+            {
+              key: '3',
+              icon: <UploadOutlined />,
+              label: 'nav 3',
+            },
+          ]}
+        />
+      </Sider>
+      <Layout className="site-layout">
+        <Header className={styles.common}>
+          {React.createElement(
+            collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+            {
+              className: classnames({ [styles.trigger]: true }),
+              onClick: () => setCollapsed(!collapsed),
+            },
+          )}
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default App;
